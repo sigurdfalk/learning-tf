e18dc0b21614de29b63e66c7a4e0414ae334282b
Sigurd Hagen Falk <sigurd.falk@gjensidige.no>
2020-02-07 08:18:28 +0100

    module 7.48: refactoring into a module

--- main.tf
+++ main.tf
@@ -1,25 +1,17 @@
+# Environment vars START
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
+# Environment vars END
 
-variable "web_server_location" {}
 variable "web_server_rg" {}
 variable "resource_prefix" {}
-variable "web_server_address_space" {}
 variable "web_server_name" {}
 variable "environment" {}
 variable "web_server_count" {}
-variable "web_server_subnets" {
-    type = "list"
-}
 variable "terraform_script_version" {}
 variable "domain_name_label" {}
-
-locals {
-    web_server_name     = var.environment == "production" ? "${var.web_server_name}-prd" : "${var.web_server_name}-dev"
-    build_environment   = var.environment == "production" ? "production" : "development"
-}
 
 provider "azurerm" {
     version         = "=1.43.0"
@@ -29,159 +21,32 @@ provider "azurerm" {
     subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "web_server_rg" {
-    name        = var.web_server_rg
-    location    = var.web_server_location
-
-    tags = {
-        environment = local.build_environment
-        version     = var.terraform_script_version
-    }
+module "location_us2w" {
+    source = "./location"
+    
+    web_server_location         = "westus2"
+    web_server_rg               = "${var.web_server_rg}-us2w"
+    resource_prefix             = "${var.resource_prefix}-us2w"
+    web_server_address_space    = "1.0.0.0/22"
+    web_server_name             = var.web_server_name
+    environment                 = var.environment
+    web_server_count            = var.web_server_count
+    web_server_subnets          = ["1.0.1.0/24", "1.0.2.0/24"]
+    domain_name_label           = var.domain_name_label
+    terraform_script_version    = var.terraform_script_version
 }
 
-resource "azurerm_virtual_network" "web_server_vnet" {
-    name                = "${var.resource_prefix}-vnet"
-    location            = var.web_server_location
-    resource_group_name = azurerm_resource_group.web_server_rg.name
-    address_space       = [var.web_server_address_space]
+module "location_eu1w" {
+    source = "./location"
+    
+    web_server_location         = "westeurope"
+    web_server_rg               = "${var.web_server_rg}-eu1w"
+    resource_prefix             = "${var.resource_prefix}-eu1w"
+    web_server_address_space    = "2.0.0.0/22"
+    web_server_name             = var.web_server_name
+    environment                 = var.environment
+    web_server_count            = var.web_server_count
+    web_server_subnets          = ["2.0.1.0/24", "2.0.2.0/24"]
+    domain_name_label           = var.domain_name_label
+    terraform_script_version    = var.terraform_script_version
 }
-
-resource "azurerm_subnet" "web_server_subnet" {
-    name                        = "${var.resource_prefix}-${substr(var.web_server_subnets[count.index], 0, length(var.web_server_subnets[count.index]) - 3)}-subnet"
-    resource_group_name         = azurerm_resource_group.web_server_rg.name
-    virtual_network_name        = azurerm_virtual_network.web_server_vnet.name
-    address_prefix              = var.web_server_subnets[count.index]
-    network_security_group_id   = count.index == 0 ? azurerm_network_security_group.web_server_nsg.id : ""
-    count                       = length(var.web_server_subnets)
-}
-
-resource "azurerm_public_ip" "web_server_lb_public_ip" {
-    name                            = "${var.resource_prefix}-public-ip"
-    location                        = var.web_server_location
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-    allocation_method               = var.environment == "production" ? "Static" : "Dynamic"
-    domain_name_label               = var.domain_name_label
-}
-
-resource "azurerm_network_security_group" "web_server_nsg" {
-    name                            = "${var.resource_prefix}-nsg"
-    location                        = var.web_server_location
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-}
-
-resource "azurerm_network_security_rule" "web_server_nsg_rule_http" {
-    name                            = "HTTP Inbound" 
-    priority                        = 100
-    direction                       = "Inbound"
-    access                          = "Allow"
-    protocol                        = "TCP"
-    source_port_range               = "*"
-    destination_port_range          = "80"
-    source_address_prefix           = "*"
-    destination_address_prefix      = "*"
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-    network_security_group_name     = azurerm_network_security_group.web_server_nsg.name
-}
-
-resource "azurerm_virtual_machine_scale_set" "web_sever" {
-    name                            = "${local.web_server_name}-scale-set"
-    location                        = var.web_server_location
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-    upgrade_policy_mode             = "manual"
-
-    sku {
-        name        = "Standard_B1s"
-        tier        = "Standard"
-        capacity    = var.web_server_count
-    }
-
-    storage_profile_image_reference {
-        publisher   = "MicrosoftWindowsServer"
-        offer       = "WindowsServer"
-        sku         = "2016-Datacenter-Server-Core-smalldisk"
-        version     = "latest"
-    }
-
-    storage_profile_os_disk {
-        name                = "" # Auto generated
-        caching             = "ReadWrite"
-        create_option       = "FromImage"
-        managed_disk_type   = "Standard_LRS" # Locally Redundant Storage
-    }
-
-    os_profile {
-        computer_name_prefix    = local.web_server_name
-        admin_username          = "webserver"
-        admin_password          = "Passw0rd1234"
-    }
-
-    os_profile_windows_config {
-        provision_vm_agent = true
-    }
-
-    network_profile {
-        name    = "web_server_network_profile"
-        primary = true
-
-        ip_configuration {
-            name                                    = local.web_server_name
-            primary                                 = true
-            subnet_id                               = azurerm_subnet.web_server_subnet.*.id[0]
-            load_balancer_backend_address_pool_ids  = [azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id]
-        }
-    }
-
-    extension {
-        name                    = "${local.web_server_name}-extension"
-        publisher               = "Microsoft.Compute"
-        type                    = "CustomScriptExtension"
-        type_handler_version    = "1.9"
-        settings                = <<SETTINGS
-        {
-            "fileUris": ["https://raw.githubusercontent.com/eltimmo/learning/master/azureInstallWebServer.ps1"],
-            "commandToExecute": "start powershell -ExecutionPolicy Unrestricted -File  azureInstallWebServer.ps1"
-        }
-        SETTINGS
-    }
-}
-
-resource "azurerm_lb" "web_server_lb" {
-    name                            = "${var.resource_prefix}-lb"
-    location                        = var.web_server_location
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-
-    frontend_ip_configuration {
-        name                            = "${var.resource_prefix}-lb-frontend-ip"
-        public_ip_address_id            = azurerm_public_ip.web_server_lb_public_ip.id
-    }
-}
-
-resource "azurerm_lb_backend_address_pool" "web_server_lb_backend_pool" {
-    name                            = "${var.resource_prefix}-lb-backend-pool"
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-    loadbalancer_id                 = azurerm_lb.web_server_lb.id
-}
-
-resource "azurerm_lb_probe" "web_server_lb_http_probe" {
-    name                            = "${var.resource_prefix}-lb-http-probe"
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-    loadbalancer_id                 = azurerm_lb.web_server_lb.id
-    protocol                        = "tcp"
-    port                            = "80"
-}
-
-resource "azurerm_lb_rule" "web_server_lb_http_rule" {
-    name                            = "${var.resource_prefix}-lb-http-rule"
-    resource_group_name             = azurerm_resource_group.web_server_rg.name
-    loadbalancer_id                 = azurerm_lb.web_server_lb.id
-    protocol                        = "tcp"
-    frontend_port                   = "80"
-    backend_port                    = "80"
-    frontend_ip_configuration_name  = "${var.resource_prefix}-lb-frontend-ip"
-    probe_id                        = azurerm_lb_probe.web_server_lb_http_probe.id
-    backend_address_pool_id         = azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id
-}
-
-
-
-
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,7 @@
-web_server_location         = "westus2"
 web_server_rg               = "web-rg"
 resource_prefix             = "web-server"
-web_server_address_space    = "1.0.0.0/22"
 web_server_name             = "web"
 environment                 = "x" 
 web_server_count            = 2
-web_server_subnets          = ["1.0.1.0/24", "1.0.2.0/24"]
 terraform_script_version    = "1.00"
 domain_name_label           = "learning-tf-web"
